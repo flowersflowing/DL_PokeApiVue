788f414971fca3b1b5e28903cd46477b2f1ae1f2
Natalia <nflores.teoria@gmail.com>
2020-11-12 16:26:24 -0300

    Agrego objetos de la data con las variables vacías, más el div que va a devolver la info

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,25 +1,51 @@
 <template>
   <section class="container">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" alt="Pokemón">
-    <form @submit.prevent="">
+    <div class="text-center my-5">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" alt="Pokemón">      
+    </div>
+    <form @submit.prevent="traerpokemon()">
       <div class="form-group">
-        <label for="formGroupExampleInput">Ingresa el nombre de tu pokemón</label>
+        <label for="formGroupExampleInput">Ingresa el nombre de tu pokemón:</label>
         <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Bulbasaur" v-model="nombre_pokemon">
       </div>
-      <button type="submit" class="btn btn-danger">Buscar</button>
+      <div class="text-right">
+        <button type="submit" class="btn btn-danger">Buscar</button>        
+      </div>
     </form>
     <!-- Mostrar nombre, imagen, etc. -->
+    <div class="text-center my-5">
+      <img src="" alt="">
+      <h3>Movimientos</h3>
+      <ul>
+        <li></li>
+      </ul>
+      <h3>Habilidades</h3>
+      <ul>
+        <li></li>
+      </ul>      
+    </div>
   </section>
 </template>
 
 <script>
 export default {
   name: 'Formulario',
-  // data() {
-  //   return {
-  //     nombre_pokemon
-  //   }
-  // }
+  data() {
+    return {
+      nombre_pokemon: '',
+      imagen: '',
+      movimientos: [],
+      habilidades: [],
+    }
+  },
+  methods: {
+    traerpokemon() {
+      // hacer el llamado a la API
+    }
+  },
+  created() {
+    //Creo que aquí debería ir el pikachu que es el que debe venir predeterminado
+  },
 }
 </script>
 
@@ -27,6 +53,8 @@ export default {
 <style scoped>
   img {
     height: 180px;
-    padding: 3em;
+  }
+  li {
+    list-style-type: none;
   }
 </style>
